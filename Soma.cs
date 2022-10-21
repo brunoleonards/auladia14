abfa36e64a0a7a7585505948c37daea49fd2002c
brunoleonards <114749297+brunoleonards@users.noreply.github.com>
2022-10-21 09:43:59 -0300

    Add files via upload

---
+++ Soma.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace auladia14
+{
+    internal class Soma
+    {
+        public int numero1;
+        public int numero2;
+
+
+        internal double somar(int numero1,int numero2);
+
+    }
+
+}
